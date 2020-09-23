0577db4b5f49ef09ded80b1953e1f0c0325bf631
Oleksii Kovalenko <o.kovalenko@pozitiff.ua>
2020-09-23 14:34:18 +0300

    NotFound component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const NotFound = () => (
-  <div>
-    <h1>404 - Not Found!</h1>
-    <Link to="/">Go Home</Link>
+  <div className="not-found">
+    <h1 className="not-found__title">404 - Not Found!</h1>
+    <Link className="not-found__link" to="/">
+      Go Home
+    </Link>
   </div>
 )
 
