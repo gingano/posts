d0785f26a4012e786d2db262882da260696a22f0
Oleksii Kovalenko <o.kovalenko@pozitiff.ua>
2020-09-23 14:40:25 +0300

    NotFound preloader fix

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { setIsVisible } from '../redux/actions/preloader'
 
-const NotFound = () => (
-  <div className="not-found">
-    <h1 className="not-found__title">404 - Not Found!</h1>
-    <Link className="not-found__link" to="/">
-      Go Home
-    </Link>
-  </div>
-)
+const NotFound = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(setIsVisible(false))
+  }, [])
+
+  return (
+    <div className="not-found">
+      <h1 className="not-found__title">404 - Not Found!</h1>
+      <Link className="not-found__link" to="/">
+        Go Home
+      </Link>
+    </div>
+  )
+}
 
 export default NotFound
